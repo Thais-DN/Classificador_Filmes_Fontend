2c2245bf0dde1991f462d5f0db9f488669dd6037
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-25 14:22:09 -0300

    incluindo busca ao grupo

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,42 +1,49 @@
-import React, { useState } from "react";
-import useFetchSearch from "../../hooks/useFetchSearch";
+import React, { useState, useEffect } from "react";
+import { GrupoObject } from "../../types";
 
-const Search = () => {
+interface SearchProps {
+    grupos: GrupoObject[];
+    onSearch: (filteredGrupos: GrupoObject[]) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ grupos, onSearch }) => {
     const [searchTerm, setSearchTerm] = useState("");
-    const { data, loading, error } = useFetchSearch(searchTerm);
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(data);
-    };
+    useEffect(() => {
+        const normalizedSearchTerm = searchTerm
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .toLowerCase();
+
+        const filtered = grupos.filter((grupo) =>
+            grupo.nome
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .toLowerCase()
+                .includes(normalizedSearchTerm)
+        );
+        onSearch(filtered);
+    }, [searchTerm, grupos, onSearch]);
 
     return (
         <div className="flex items-center w-full justify-center p-5">
-            <form
-                onSubmit={handleSearch}
-                className="flex gap-2 w-4/5 border-purple-200 rounded"
-            >
+            <form className="flex gap-2 w-4/5 border-purple-200 rounded">
                 <input
                     type="text"
                     className="block w-full px-4 py-2 text-slate-950 bg-slate-00 border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder:text-slate-700"
-                    placeholder="Busque sua animação..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Busque sua animação..."
                 />
                 <button
                     type="submit"
-                    className="px-4 text-slate-100 bg-slate-900 hover:bg-slate-700 border-l rounded "
+                    className="px-4 text-slate-100 bg-slate-900 border-l rounded"
                 >
                     Buscar
                 </button>
             </form>
-            {loading && <p>Carregando...</p>}
-            {error && <p>Ocorreu um erro: {error.message}</p>}
-            {/* Renderização condicional dos resultados da pesquisa */}
-            {!loading && !error && data && (
-                <div>{/* Renderize os resultados da pesquisa aqui */}</div>
-            )}
         </div>
     );
 };
+
 export default Search;
--- src/pages/GruposFilmes.tsx
+++ src/pages/GruposFilmes.tsx
@@ -2,9 +2,12 @@ import CardGrupo from "../components/CardGrupo/CardGrupo";
 import useFetchGrupos from "../hooks/useFetchGrupos";
 import mickey from "/images/mickey.png";
 import Search from "../components/Search/Search";
+import { useState } from "react";
+import { GrupoObject } from "../types";
 
 function GruposFilmes() {
     const { data: grupos, error, loading } = useFetchGrupos();
+    const [filteredGrupos, setFilteredGrupos] = useState<GrupoObject[]>([]);
 
     if (loading) {
         return <div>Carregando...</div>;
@@ -23,11 +26,11 @@ function GruposFilmes() {
 
     return (
         <div style={fundoMickey}>
-            <Search />
+            <Search grupos={grupos || []} onSearch={setFilteredGrupos} />
             <div className="grid justify-center grid-cols-1 gap-16 px-2 py-10 md:p-10 md:grid-cols-2 xl:grid-cols-3">
-                {grupos?.map((grupo, index) => {
-                    return <CardGrupo grupo={grupo} key={index} />;
-                })}
+                {filteredGrupos.map((grupo) => (
+                    <CardGrupo key={grupo.id} grupo={grupo} />
+                ))}
             </div>
         </div>
     );
